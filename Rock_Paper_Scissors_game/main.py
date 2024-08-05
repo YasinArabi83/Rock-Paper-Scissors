9f8957df929c16d76d6df128cc233643b2abc2f8
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-06 00:33:07 +0330

    Create a connection to the database

--- Rock_Paper_Scissors_game/main.py
+++ Rock_Paper_Scissors_game/main.py
@@ -1,8 +1,20 @@
-import getpass
-from Rock_Paper_Scissors_game.ManagerGame import ManagerGame
-from User import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from User import User, Base
+from Game import Game,Base
+from UserManager import UserManager
+from GameManager import GameManager
 
-Users: list[User] = []
+# Create a connection to the database
+engine = create_engine('sqlite:///game.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
+user_manager = UserManager(session)
+game_manager = GameManager(session)
 
 while True:
     print(" =========================== \n"
@@ -10,7 +22,8 @@ while True:
           "1. Create new user \n"
           "2. Start new match \n"
           "3. Get users list\n"
-          "4. Exit\n\n"
+          "4. Get games list\n"
+          "5. Exit\n\n"
           "===========================")
     choice = input("What`s your choice: ")
     
