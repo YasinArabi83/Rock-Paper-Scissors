8f55130e6cf1dbd7b3a61679aae8af42a755f46d
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-06 00:14:34 +0330

    Update PlayerSelector func

--- Rock_Paper_Scissors_game/UserManager.py
+++ Rock_Paper_Scissors_game/UserManager.py
@@ -10,17 +10,20 @@ class UserManager:
         self.session.add(new_user)
         self.session.commit()
         print(f"User created: {username}")
-        
-    @staticmethod
-    def SelectUser(UserList: list):
-        while True:
-            user_id_input = input("\nEnter user ID: ")
-            Id = int(user_id_input) if user_id_input.isdecimal() else -1
 
-            if 0 <= Id < len(UserList):
-                return UserList[Id]
+    
+    def PlayerSelector(self,users: list):
+        while True:
+            print("Select Player :")
+            for i, user in enumerate(users,1):
+                print(f"{i }. {user.name}")
+             
+            user_input = input("Enter the number of Player 1: ")
+            user_index=int(user_input) if user_input.isdecimal() else -1
+            if len(users) >=user_index>=0:
+                return users[user_index-1]
             else:
-                print("\nInvalid selection. Please select again\n")
+                print(f"Invalid Input , Please enter a number between 1 and  + {len(users)}")
 
     @staticmethod
     def PrintUserList(UserList: list):
