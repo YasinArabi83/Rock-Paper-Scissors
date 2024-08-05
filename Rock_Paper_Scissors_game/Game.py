1782ca20d5b4a575b2aa19b3671808f93062f3ed
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-05 20:05:32 +0330

    Making it possible to save games by adding the Game class

---
+++ Rock_Paper_Scissors_game/Game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, id, player1_id, player2_id,winner_id):
+        self.id = id
+        self.player1_id = player1_id
+        self.player2_id = player2_id
+        self.winner_id = winner_id
