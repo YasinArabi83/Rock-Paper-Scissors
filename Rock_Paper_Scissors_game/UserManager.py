3e72ec463773fbc9e5ab6a34623a78969e3d2098
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-06 01:39:58 +0330

    Correcting some spelling mistakes

--- Rock_Paper_Scissors_game/UserManager.py
+++ Rock_Paper_Scissors_game/UserManager.py
@@ -18,12 +18,12 @@ class UserManager:
             for i, user in enumerate(users,1):
                 print(f"{i }. {user.name}")
              
-            user_input = input("Enter the number of Player 1: ")
+            user_input = input("Enter the number of Player : ")
             user_index=int(user_input) if user_input.isdecimal() else -1
             if len(users) >=user_index>=0:
                 return users[user_index-1]
             else:
-                print(f"Invalid Input , Please enter a number between 1 and  + {len(users)}")
+                print(f"Invalid Input , Please enter a number between 1 and   {len(users)}")
 
     def GetUsers(self):
         users = self.session.query(User).all()
